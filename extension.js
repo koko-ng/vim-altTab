54276789bb7612999d88d32d1fa8550829468cfa
koko-ng <koko-ng@users.noreply.github.com>
2023-09-17 16:20:32 +0200

    gnome 45

--- extension.js
+++ extension.js
@@ -1,51 +1,51 @@
 /* -*- mode: js; js-basic-offset: 4; indent-tabs-mode: nil -*- */
 
-const Clutter = imports.gi.Clutter;
-const Meta = imports.gi.Meta;
-const Shell = imports.gi.Shell;
+import Clutter from "gi://Clutter";
 
-const AltTab = imports.ui.altTab;
-const Main = imports.ui.main;
-const WindowManager = imports.ui.windowManager;
+import * as AltTab from "resource:///org/gnome/shell/ui/altTab.js";
 
 let injections = {
-	'WindowSwitcherPopup': {},
-	'AppSwitcherPopup': {}
+  WindowSwitcherPopup: {},
+  AppSwitcherPopup: {},
 };
 
-function init(metadata) {
-}
+function init(metadata) {}
 
 function mapVimKeys(cb) {
-	return function(keysym, action) {
-		switch(keysym) {
-			case Clutter.KEY_h:
-				keysym = Clutter.Left || Clutter.KEY_Left;
-				break;
-			case Clutter.KEY_l:
-				keysym = Clutter.Right || Clutter.KEY_Right;
-				break;
-			case Clutter.KEY_k:
-				keysym = Clutter.Up || Clutter.KEY_Up;
-				break;
-			case Clutter.KEY_j:
-				keysym = Clutter.Down || Clutter.KEY_Down;
-				break;
-		}
-		cb.call(this, keysym, action);
-	}
+  return function (keysym, action) {
+    switch (keysym) {
+      case Clutter.KEY_h:
+        keysym = Clutter.Left || Clutter.KEY_Left;
+        break;
+      case Clutter.KEY_l:
+        keysym = Clutter.Right || Clutter.KEY_Right;
+        break;
+      case Clutter.KEY_k:
+        keysym = Clutter.Up || Clutter.KEY_Up;
+        break;
+      case Clutter.KEY_j:
+        keysym = Clutter.Down || Clutter.KEY_Down;
+        break;
+    }
+    cb.call(this, keysym, action);
+  };
 }
 
-function enable() {
-	for (let switcherPopup in injections) {
-		injections[switcherPopup]['_keyPressHandler'] = AltTab[switcherPopup].prototype._keyPressHandler;
-		AltTab[switcherPopup].prototype._keyPressHandler = mapVimKeys(injections[switcherPopup]['_keyPressHandler']);
-	}
-}
+export default class VimAltTab {
+  enable() {
+    for (let switcherPopup in injections) {
+      injections[switcherPopup]["_keyPressHandler"] =
+        AltTab[switcherPopup].prototype._keyPressHandler;
+      AltTab[switcherPopup].prototype._keyPressHandler = mapVimKeys(
+        injections[switcherPopup]["_keyPressHandler"]
+      );
+    }
+  }
 
-function disable() {
-	for (let switcherPopup in injections) {
-		for (let prop in switcherPopup )
-			AltTab[switcherPopup].prototype[prop] = switcherPopup[prop];
-	}
+  disable() {
+    for (let switcherPopup in injections) {
+      for (let prop in switcherPopup)
+        AltTab[switcherPopup].prototype[prop] = switcherPopup[prop];
+    }
+  }
 }
